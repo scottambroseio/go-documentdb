0f95f8ae67eff195ed4dc11c09e47b155ad46b5c
Scott Ranger <millenniumnight@googlemail.com>
2018-04-06 20:09:42 +0000

    Delete database

--- main.go
+++ main.go
@@ -8,6 +8,56 @@ import "net/http"
 import "io/ioutil"
 import "os"
 
+func DeleteDatabase(id string) string {
+	key := os.Getenv("CosmosKey")
+	url := os.Getenv("CosmosUrl") + fmt.Sprintf("/dbs/%v", id)
+
+	date := strings.Replace(time.Now().Format(time.RFC1123), "UTC", "GMT", 1)
+
+	provider := MasterKeyTokenProvider{
+		Verb:         "DELETE",
+		ResourceType: "dbs",
+		ResourceLink: fmt.Sprintf("dbs/%v", id),
+		Date:         date,
+		Key:          key,
+		KeyType:      "master",
+		TokenVersion: "1.0",
+	}
+
+	signature, err := provider.GenerateToken()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("DELETE", url, nil)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	req.Header.Set("Authorization", signature)
+	req.Header.Set("x-ms-date", date)
+	req.Header.Set("x-ms-version", "2015-08-06")
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer res.Body.Close()
+
+	bytes, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return string(bytes)
+}
+
 func CreateDatabase(id string) string {
 	key := os.Getenv("CosmosKey")
 	url := os.Getenv("CosmosUrl") + "/dbs"
@@ -58,6 +108,7 @@ func CreateDatabase(id string) string {
 
 	return string(bytes)
 }
+
 func GetDatabases() string {
 	key := os.Getenv("CosmosKey")
 	url := os.Getenv("CosmosUrl") + "/dbs"
@@ -108,9 +159,4 @@ func GetDatabases() string {
 }
 
 func main() {
-	fmt.Println(GetDatabases())
-	fmt.Println()
-	fmt.Println(CreateDatabase("scott-cdb-db2"))
-	fmt.Println()
-	fmt.Println(GetDatabases())
 }
